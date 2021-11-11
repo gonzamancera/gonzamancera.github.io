234e4c5b7f1b27d38bda17ddfa87b43fb2ea8c41
gonzamancera <gonzalomancera@DESKTOP-O0P9JQP.localdomain>
2021-11-11 19:23:23 +0100

    Agregué cambios a mi repo!

---
+++ codigo.js
@@ -0,0 +1,7 @@
+function addItem(){
+    const cantidad = document.getElementById("input").value
+    const tipo = document.getElementById("options").value
+    console.log(cantidad)
+    console.log(tipo)
+    }
+    
