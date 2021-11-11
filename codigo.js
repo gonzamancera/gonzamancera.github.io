b2a00f5f0a3ddcd3bc00d598068618fb076003b7
PabloCancela77 <pablo.cancela@estudiante.uam.es>
2021-11-11 19:56:48 +0100

    zapatillas

--- codigo.js
+++ codigo.js
@@ -1,7 +1,19 @@
 function addItem(){
-    const cantidad = document.getElementById("input").value
-    const tipo = document.getElementById("options").value
+    const cantidad = document.getElementById("options").value
+    const tipo = document.getElementById("input").value
     console.log(cantidad)
     console.log(tipo)
+
+    const fila = document.createElement("tr")
+    const c1 = document.createElement("td")
+    c1.innerText = cantidad
+    const c2 = document.createElement("td")
+    c2.innerText = tipo
+    fila.appendChild(c1)
+    fila.appendChild(c2)
+
+    //localizo table body:
+    const table = document.getElementById("items")
+    table.appendChild(fila)
     }
     
